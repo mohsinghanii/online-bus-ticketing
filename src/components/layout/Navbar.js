d7cb43f05cf78d73c725f293e8ba981bc3757b37
mohsinghani <mohsinghani.777@gmail.com>
2018-07-17 20:41:45 +0500

    change background and content

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -60,7 +60,7 @@ class Navbar extends Component {
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            Website.com
+            Tourist World
           </Link>
           <button
             className="navbar-toggler"
@@ -73,12 +73,12 @@ class Navbar extends Component {
 
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
+              {/* <li className="nav-item">
                 <Link className="nav-link" to="/profiles">
                   {' '}
                   People
                 </Link>
-              </li>
+              </li> */}
             </ul>
             {isAuthenticated ? authLinks : guestLinks}
           </div>
