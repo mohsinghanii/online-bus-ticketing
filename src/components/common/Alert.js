188c214905444fe2244ba1e4c7445d5741e9fe8d
mughees605 <mughees605@gmail.com>
2018-07-25 20:59:50 +0500

    remove dissmissal removed temporary

--- src/components/common/Alert.js
+++ src/components/common/Alert.js
@@ -4,9 +4,9 @@ import React from 'react';
 export const Alert = ({isError , errorMessage, type}) => {
  return (
     isError && 
-        <div className={"alert alert-" + type + " alert-dismissible mt10"}>
+        <div className={"alert alert-" + type + " mt10"}>
           {errorMessage}
-          <button type="button" className="close" onClick={this.handleClick} data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <button type="button" className="close" onClick={this.handleClick} ><span aria-hidden="true">&times;</span></button>
        </div>
  )
 }
