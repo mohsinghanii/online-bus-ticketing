e14235ed66af1243d6e69edf2c0355b75f27ff1f
mughees605 <mughees605@gmail.com>
2018-07-25 20:59:30 +0500

    loading component

---
+++ src/components/common/loadingComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactLoading from 'react-loading';
+
+export const Loading = ({isLoading, type, color, height = "50px", width = "50px"}) => {
+    debugger
+  return (
+      isLoading ?
+        <div className="flexAndCenter">
+             <ReactLoading type={type} color={color} height={height} width={width} />
+        </div>
+      :
+      ''
+  )
+}
