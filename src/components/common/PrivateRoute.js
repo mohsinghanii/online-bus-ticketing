86f838b8f5da039793547870df3efe477f0be8d1
mughees605 <mughees605@gmail.com>
2018-07-28 21:27:03 +0500

    minor update

--- src/components/common/PrivateRoute.js
+++ src/components/common/PrivateRoute.js
@@ -3,26 +3,27 @@ import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-const PrivateRoute = ({ component: Component, auth, ...rest }) => (
+const PrivateRoute = ({ component: Component, auth, ...rest }) => {
+  return (
     <Route
     {...rest}
     render = {props =>
-        // auth.isAuthenticated === true ? (
-        true ? (
+       true ? (
             <Component {...props} />
         ) : (
             <Redirect to="/login" />
         )
     }
     />
-);
+  )
+}
 
 PrivateRoute.propTypes = {
   auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  auth: state.auth
+  auth: state.userAuth.user
 });
 
 export default connect(mapStateToProps)(PrivateRoute);
