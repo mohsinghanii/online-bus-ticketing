26f32d1dfbd06fde05c8223bac270426705b57d5
mughees605 <mughees605@gmail.com>
2018-07-30 02:33:03 +0500

    minor change in firebase build

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const prodConfig = {
-//   apiKey: YOUR_API_KEY,
-//   authDomain: YOUR_AUTH_DOMAIN,
-//   databaseURL: YOUR_DATABASE_URL,
-//   projectId: YOUR_PROJECT_ID,
-//   storageBucket: '',
-//   messagingSenderId: YOUR_MESSAGING_SENDER_ID,
+  apiKey: "AIzaSyBoY_l1OQXzNY83yv-3OudIbs8_Jg1A12M",
+  authDomain: "karachi-online-bus.firebaseapp.com",
+  databaseURL: "https://karachi-online-bus.firebaseio.com",
+  projectId: "karachi-online-bus",
+  storageBucket: "karachi-online-bus.appspot.com",
+  messagingSenderId: "279553797235"
 };
 
 const devConfig = {
