aaba3368a21a493e753c587d3bb3b783a946d587
mughees605 <mughees605@gmail.com>
2018-08-06 03:05:14 +0500

    busreducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,11 +1,13 @@
-import { combineReducers } from 'redux'
-import userAuthReducer from './userReducer'
-import CompanyReducer from './CompanyReducer'
+import { combineReducers } from 'redux';
+import userAuthReducer from './userReducer';
+import CompanyReducer from './CompanyReducer';
+import BusReducer from './BusReducer';
 
 
 const rootReducer = combineReducers({
     userAuth: userAuthReducer,
     CompanyReducer,
+    BusReducer
 })
 
 export default rootReducer
