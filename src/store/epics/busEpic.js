859c66fd76fbafe0b3543733dd576e4150e8ab23
mughees605 <mughees605@gmail.com>
2018-08-06 03:01:33 +0500

    bus epic

---
+++ src/store/epics/busEpic.js
@@ -0,0 +1,30 @@
+import {
+CREATE_BUS
+} from '../constants';
+import { doCreateBusInCompany } from './../../firebase/db';
+import { Observable } from 'rxjs/Rx';
+import { BusAction } from './../actions/index'
+
+export default class BusEpic {
+
+    static createBus = (action$) =>
+        action$.ofType(CREATE_BUS)
+            .switchMap(({ payload }) => {
+                debugger
+                const { bid, cid, bus_name, date_created } = payload
+                return Observable.fromPromise(doCreateBusInCompany(bid, cid, bus_name, date_created))
+                   .switchMap((response)=>{
+                       debugger
+                      return Observable.of(
+                          BusAction.createBusSuccess(response)
+                      )
+                   })
+                   .catch((err)=>{
+                      return Observable.of(
+                          BusAction.createBusFailure(err.message)
+                      )
+                   })
+            })
+
+}
+
