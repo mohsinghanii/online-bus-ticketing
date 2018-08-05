8bae32c667f4ae0c3f6118128af1f8a831e25c36
mughees605 <mughees605@gmail.com>
2018-08-06 03:05:06 +0500

    busepic

--- src/store/epics/index.js
+++ src/store/epics/index.js
@@ -1,6 +1,7 @@
 import { combineEpics } from 'redux-observable';
 import AuthEpic from './userEpic';
 import CompanyEpic from './CompanyEpic';
+import BusEpic from './busEpic';
 
 const rootEpic = combineEpics(
     AuthEpic.signUpEpic,
@@ -9,6 +10,9 @@ const rootEpic = combineEpics(
     CompanyEpic.createCompany,
     CompanyEpic.getCompanies,
     CompanyEpic.getCompany,
+    //Buses
+    BusEpic.createBus,
+
 );
 
 export default rootEpic;
