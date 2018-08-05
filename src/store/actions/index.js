125329f7b46d12d054f35bcb25761779298fca87
mughees605 <mughees605@gmail.com>
2018-08-06 03:04:25 +0500

    bus action in index.js

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,7 +1,9 @@
 import { userAuthActions } from './userAction';
 import { CompanyAction } from './CompanyAction';
+import { BusAction } from './busAction';
 
 export {
     userAuthActions,
+    BusAction,
     CompanyAction
 }
