d4f0b71b1754f5864f8716000017e069cf368a38
mughees605 <mughees605@gmail.com>
2018-08-06 03:03:18 +0500

    create bus in company firestore handler

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -11,3 +11,30 @@ export const doCreateUser = (id, username, email) =>
 
 export const onceGetUsers = () =>
   db.ref('users').once('value');
+
+// bus query
+export const doCreateBusInCompany = (bid, cid, bus_name, date_created) =>
+  new Promise((res, rej) => {
+    const ref = firestoreDb.collection("Buses").doc(bid)
+    ref.set({
+      bid,
+      cid,
+      bus_name,
+      date_created
+    })
+      .then(() => {
+        ref.get().then((doc) => {
+          alert(doc.data())
+          res(doc.data())
+        })
+          .catch((err) => {
+            alert(err)
+            rej(err)
+          })
+      })
+      .catch((err) => {
+        alert(err)
+        rej(err)
+      })
+
+  })
